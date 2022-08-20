b78b3862f59aa62f1a0c24f1a8fb3847a2a8e9a7
obmelyukhin <polozhitelnyy88@mail.ru>
2022-08-20 20:38:54 +0300

    Lesson 7.1

---
+++ src/main/java/Lesson7/Maximum.java
@@ -0,0 +1,28 @@
+package Lesson7;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class Maximum {
+
+    @JsonProperty(value = "Value")
+    private int value;
+    @JsonProperty(value = "Unit")
+    private String unit;
+    @JsonProperty(value = "UnitType")
+    private String unitType;
+
+    int getValue() {
+        int s;
+        s = (int) ((value - 32)/1.8);
+        return s;
+    }
+}
